9e4d0e000a51672a5be5f824752913c261c23bb5
Jzy <jestalycastillo15@gmail.com>
2025-02-15 00:02:16 +0800

    solved leetcode #1431

--- ARRAYS/Array22/Main.java
+++ ARRAYS/Array22/Main.java
@@ -7,24 +7,28 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
 
-        int[] candies = { 12, 1, 12 };
-        int extraCandies = 3;
+        int[] candies = { 4, 2, 1, 1, 2 };
+        int extraCandies = 1;
         System.out.println(main.kidsWithCandies(candies, extraCandies));
     }
 
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> list = new ArrayList<>();
-
-        for (int i = 0; i < candies.length; i++) {
-            for (int j = 0; j < i - 1; j++) {
-                if (candies[j] < candies[i]) {
-                    list.add(false);
-                    break;
-                }
+        int max = candies[0];
+        for (int i = 1; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
             }
-            list.add(true);
         }
-
+        for (int i = 0; i < candies.length; i++) {
+            int sum = candies[i] + extraCandies;
+            if (sum >= max) {
+                list.add(true);
+                continue;
+            }
+            list.add(false);
+        }
         return list;
     }
+
 }
