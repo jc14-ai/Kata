8ff4b92fa2b2035d46a1f220ec48719c5d1e59e4
Jzy <jestalycastillo15@gmail.com>
2025-03-27 16:59:36 +0800

    solved leetcode #1528

---
+++ PROBLEMS/Problem49/Main.java
@@ -0,0 +1,24 @@
+package PROBLEMS.Problem49;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(restoreString("codeleet", new int[] { 4, 5, 6, 7, 0, 2, 1, 3 }));
+    }
+
+    public static String restoreString(String s, int[] indices) {
+        HashMap<Integer, Character> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < indices.length; i++) {
+            map.put(indices[i], s.charAt(i));
+        }
+
+        for (int i = 0; i < indices.length; i++) {
+            sb.append(map.get(i));
+        }
+
+        return sb.toString();
+    }
+}
