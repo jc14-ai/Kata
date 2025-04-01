aefa69c35c35bef2521d942504177fb2cf99173f
Jzy <jestalycastillo15@gmail.com>
2025-04-01 22:01:13 +0800

    solved leetcode #1313

--- ARRAYS/Array36/Main.java
+++ ARRAYS/Array36/Main.java
@@ -13,6 +13,7 @@ public class Main {
         for (int i = 0; i < nums.length - 1; i += 2) {
             int freq = nums[i];
             int val = nums[i + 1];
+
             for (int j = 0; j < freq; j++) {
                 list.add(val);
             }
